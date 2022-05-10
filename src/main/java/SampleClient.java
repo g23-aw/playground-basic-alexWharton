34519ad08883f75629a68c7f055ee5a2193b94a1
Alex Wharton <anwharton@gmail.com>
2022-05-10 06:48:57 -0400

    basic task: printing first and last names and birthdates of patients

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,6 +1,10 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
@@ -21,7 +25,35 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
+        List<Patient> ListOfPatients = new ArrayList<Patient>();
 
+        for (int i = 0; i < response.getEntry().size(); i++) {
+             Patient pt = (Patient) response.getEntry().get(i).getResource();
+             System.out.println(pt.getName().get(0).getGivenAsSingleString());
+             System.out.println(pt.getName().get(0).getFamily());
+             System.out.println(pt.getBirthDate());
+
+             if (ListOfPatients.size() == 0) {
+                 ListOfPatients.add(pt);
+             } else {
+                 /* a binary search could be performed here for optimal speed, but
+                  * I am assuming that speed is not a concern here.
+                  */
+                 for (int index = 0; index < ListOfPatients.size(); index++) {
+                    if (0 <= pt.getName().get(0).getGivenAsSingleString().compareToIgnoreCase(ListOfPatients.get(index).getName().get(0).getGivenAsSingleString())) {
+                            ListOfPatients.add(index, pt);
+                        break;
+                    }
+                 }
+             }
+        }
+
+        System.out.println("Print again, but this time in alphabetical order");
+
+        for (int i = 0; i < ListOfPatients.size(); i++) {
+            System.out.println(ListOfPatients.get(i).getName().get(0).getGivenAsSingleString());
+            System.out.println(ListOfPatients.get(i).getName().get(0).getFamily());
+            System.out.println(ListOfPatients.get(i).getBirthDate());
+        }
     }
-
 }
